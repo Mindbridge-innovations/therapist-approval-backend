fe87dcf9ff3b771f14abfbf951a14aa9f10a0f77
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-05-25 14:47:15 +0300

    credential route

--- routes/router.js
+++ routes/router.js
@@ -2,8 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const { login } = require('../controllers/loginController');
+const { uploadCredential, upload } = require('../controllers/credentialController');
 
 // Login route
 router.post('/login', login);
+
+// Credential upload route
+router.post('/upload-credentials', authenticateToken, upload.single('file'), uploadCredential);
+
 
 module.exports = router;
