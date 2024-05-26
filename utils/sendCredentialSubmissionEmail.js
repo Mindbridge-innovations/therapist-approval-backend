50665ff9ad7f9c6444f9308d91eeef3bf7ab642d
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-05-26 19:52:51 +0300

    fix oversight

--- utils/sendCredentialSubmissionEmail.js
+++ utils/sendCredentialSubmissionEmail.js
@@ -9,8 +9,8 @@ const sendCredentialSubmissionEmail = async (email, firstName) => {
         instructions: "Your credentials are currently under review. We will notify you once the review process is complete.",
         button: {
           color: '#22BC66', // Optional: default is #22BC66
-          text: 'View your submission',
-          link: 'https://example.com/your-submission' // Example link, update with actual link if needed
+          text: 'Thank you for your submission',
+          link: 'https://sites.google.com/view/bse24-10/home' // Example link, update with actual link if needed
         }
       },
       outro: "If you have any questions or need assistance, feel free to reply to this email."
